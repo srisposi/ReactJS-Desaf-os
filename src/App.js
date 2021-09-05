07b818de393a461b721fcd4278848a3cf795d537
srisposi <srisposi@iiconcepcion.edu.ar>
2021-09-05 14:23:04 -0300

    update 1.1

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { NavBar } from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 export default function App() {
+    let titulo= 'Bienvenidos a Aura' 
+    let subtitulo='su comida lista para llevar'
     return  (
         <>
             <NavBar />
             <h1>Menú Inicial</h1>
-            <ItemListContainer/>
+            <ItemListContainer titulo={titulo} subtitulo={subtitulo}/>
         </>
     )
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
-export default function ItemListContainer() {
+export default function ItemListContainer(params) {
+  console.log(params)
   return (
+    <>
+    <h1>{params.titulo}</h1>
+    <h2>{params.subtitulo}</h2>  
     <ul>
       <li>Comidas frías</li>
       <li>Plato del día</li>
       <li>A temperatura ambiente</li>
     </ul>
+    </>
   )
 }
